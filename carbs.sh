72dd2c86368ecd4a4b3bd3b70a5944abf159f143
Chris <atnl@protonmail.com>
2022-09-01 09:49:41 -0400

    Update carbs.sh

--- carbs.sh
+++ carbs.sh
@@ -111,6 +111,17 @@ mv /opt/Obsidian-0.15.9.AppImage -t /usr/bin
 wget https://github.com/FreeTubeApp/FreeTube/releases/tag/v0.17.1-beta/freetube_0.17.1_amd64.deb
 sudo dpkg -i freetube_0.17.1_amd64.deb
 
+# Install Signal
+# 1. Install our official public software signing key
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+# 2. Add our repository to your list of repositories
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install signal
+sudo apt update; sudo apt install signal-desktop
 
 # Install Brave browser
 sudo apt install apt-transport-https curl
