c5cabcefd8e787e292a592f617ab741b11e594a2
Chris <atnl@protonmail.com>
2023-05-29 17:21:13 -0400

    Update drice.sh

--- drice.sh
+++ drice.sh
@@ -5,7 +5,8 @@
 sudo apt update && sudo apt upgrade;
 
 # essential pkgs
-sudo apt install git wget curl vim freetube fzf neofetch pass ripgrep xset redshift kwrite audacious mystiq kdenlive haruna kodi gimp ranger mpv simplescreenrecorder vlc net-tools sqlmap netdiscover nmap cmake fzf nodejs npm openrgb ckb-next chromium
+sudo apt install git wget curl gzip scdoc neovim zsh freetube fzf neofetch pass ripgrep xset redshift kwrite audacious mystiq kdenlive haruna kodi gimp ranger mpv simplescreenrecorder vlc net-tools sqlmap netdiscover nmap cmake fzf nodejs npm openrgb ckb-next chromium yt-dlp
+sudo apt -y install lua5.1
 
 # Install shortcut scripts, make executable then move all to /usr/bin
 curl -Ls https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/UsrBin/cookiemonster -o cookiemonster
@@ -85,28 +86,29 @@ sudo apt update
 sudo apt install brave-browser
 
 
-#  Install zsh 5.8 or higher
+echo ""
+# Download & install Electrum BTC Wallet
+# Install dependencies
+printf "\e [1;34 Download & install Electrum BTC Wallet \e[0m"
+sudo apt-get install python3-pyqt5 libsecp256k1-0 python3-cryptography
+
+echo ""
+# download package
+wget https://download.electrum.org/4.3.1/Electrum-4.3.1.tar.gz
+
+# Verify signatures
+wget https://download.electrum.org/4.3.1/Electrum-4.3.1.tar.gz.asc
+gpg --verify Electrum-4.3.1.tar.gz.asc
+
+#Install with pip
+sudo apt-get install python3-setuptools python3-pip
+python3 -m pip install --user Electrum-4.3.1.tar.gz
 
 
+# Install zsh 5.8 or higher
+# see bottom of script for instructions
 
 
-
-# change to zsh and make default shell
-chsh -s $(which zsh)
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-#echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-
-
-# import my .zshrc
-curl -Ls https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/UsrBin/.zshrc -o ~/.zshrc
-
-# source/instantiate syntax-highlighting
-source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # update to latest version of python3 and pip3
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
@@ -119,44 +121,82 @@ nproc
 sudo make install
 
 
+# Install Alacritty
+# https://github.com/alacritty/alacritty/blob/master/INSTALL.md
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+rustup override set stable
+rustup update stable
+apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+cargo build --release
 
-# Install neovim latest
-curl -Ls https://raw/NeoVim
-mv nvim-linux64.tar.gz ~/.local/bin
-cd ~/.local/bin
-tar xzvf nvim-linux64.tar.gz
-rm -fr nvim-linux64.tar.gz
-ln -s ./nvim-linux64/bin/nvim ./nvim
+# printf "\e[1;31m If  infocmp returns withou any errors, the alacritty terminfo is already installed \e[0m"
+infocmp alacritty
+echo ''
 
+# printf "\e[1;31m If it is not present already, you can install it globally with the following command \e[0m"
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+echo ''
 
-# Install nvchad
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+cd ..
 
-# go
+# Desktop Entry
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
 
-# cargo/rust
-
-
-# install alacritty
+# Import my Alacritty configuration
 curl -Ls https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/.alacritty.yml -o ~/.alacritty.yml
+curl -Ls https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/.alacritty.yml -o ~/alacritty/.alacritty.yml
 
-# git clone some repos
-
-
-
-
-# lua
+# Left to add to this script
+# Install the following........
+# burpsuite
 # monero wallet
-
-# fzf
 # goland
 # intellijIDEA
 # pycharm
 # yoserial
 # java
 # steam
-
-
-# burpsuite
-
 # mullvad
+# go
+
+# Install neovim latest
+# https://docs.rockylinux.org/books/nvchad/install_nvim/
+# curl -Ls https://raw/NeoVim
+# mv nvim-linux64.tar.gz ~/.local/bin
+# cd ~/.local/bin
+# tar xzvf nvim-linux64.tar.gz
+# rm -fr nvim-linux64.tar.gz
+# ln -s ./nvim-linux64/bin/nvim ./nvim
+#
+
+#  Install nvchad
+# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+
+
+# Lastly, set cursor speed and remap caps and escape
+xset r rate 300 80
+setxkbmap -option "caps:escape"
+xset -q | grep "Caps Lock:\s*on" && xdotool key Caps_Lock
+
+
+# change to zsh and make default shell
+# then continue running this script in the new shell
+chsh -s $(which zsh);
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+#echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+# import my .zshrc
+curl -Ls https://raw.githubusercontent.com/LinuxUser255/BashAndLinux/main/UsrBin/.zshrc -o ~/.zshrc
+
+# source/instantiate syntax-highlighting
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
